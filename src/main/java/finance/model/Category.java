4f1029f530318baa1fcd77dcac8f66cbbb83adf6
Iskander Idilov <idiloviskander@gmail.com>
2026-03-30 00:17:48 +0500

    Add Category class

---
+++ src/main/java/finance/model/Category.java
@@ -0,0 +1,28 @@
+package finance.model;
+
+public class Category {
+    private String name;
+    private int id;
+    private TransactionType type;
+
+    public Category(){
+        this.name = "Empty";
+        this.id = 0;
+        this.type = TransactionType.INCOME;
+    }
+
+    public Category(String name, int id, TransactionType type){
+        this.name = name;
+        this.id = id;
+        this.type = type;
+    }
+
+    public String getName(){return name;}
+    public void setName(String name){this.name = name;}
+
+    public int getId(){return id;}
+    public void setId(int id){this.id = id;}
+
+    public TransactionType getType(){return type;}
+    public void setType(TransactionType type){this.type = type;}
+}
