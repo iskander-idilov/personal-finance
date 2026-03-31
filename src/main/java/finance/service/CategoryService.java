9d29245cdc46bb01294ebde09bf3d720efdc22c5
Iskander Idilov <idiloviskander@gmail.com>
2026-03-31 22:55:26 +0500

    Add Category Class

---
+++ src/main/java/finance/service/CategoryService.java
@@ -0,0 +1,35 @@
+package finance.service;
+import java.util.ArrayList;
+import finance.model.Category;
+
+public class CategoryService {
+    private ArrayList<Category> categories = new ArrayList<>();
+
+    public CategoryService(){
+        this.categories = new ArrayList<>();
+    }
+
+    public CategoryService(ArrayList<Category> categories){
+        this.categories = categories;
+    }
+
+    public ArrayList<Category> getCategories(){return categories;}
+    public void setCategories(ArrayList<Category> categories){this.categories = categories;}
+
+    public void addCategory(Category category){
+          categories.add(category);
+    }
+
+    public void removeCategory(Category category){
+        categories.remove(category);
+    }
+
+    public Category findById(int id){
+        for (Category category : categories){
+            if(category.getId() == id){
+                return category;
+            }
+        }
+        return null;
+    }
+}
