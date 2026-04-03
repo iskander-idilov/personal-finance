75e98aaeea34a0653967f99276fc5525e649d53d
Iskander Idilov <idiloviskander@gmail.com>
2026-04-03 22:22:25 +0500

    Add App main class

---
+++ src/main/java/finance/App.java
@@ -0,0 +1,9 @@
+package finance;
+import finance.ui.MainMenu;
+
+public class App {
+    public static void main(String[] args){
+         MainMenu menu = new MainMenu();
+         menu.show();
+    }
+}
--- src/main/java/finance/ui/MainMenu.java
+++ src/main/java/finance/ui/MainMenu.java
@@ -20,27 +20,27 @@ public class MainMenu {
 
             switch (input) {
                 case 1:
-                    System.out.print("1. Добавить доход");
+                    System.out.println("1. Добавить доход");
                     break;
 
                 case 2:
-                    System.out.print("2. Добавить расход");
+                    System.out.println("2. Добавить расход");
                     break;
 
                 case 3:
-                    System.out.print("3. Посмотреть баланс");
+                    System.out.println("3. Посмотреть баланс");
                     break;
 
                 case 4:
-                    System.out.print("4. Категории");
+                    System.out.println("4. Категории");
                     break;
 
                 case 5:
-                    System.out.print("5. Транзакции");
+                    System.out.println("5. Транзакции");
                     break;
 
                 case 6:
-                    System.out.print("Удачного дня!");
+                    System.out.println("Удачного дня!");
                     return;
             }
         }
