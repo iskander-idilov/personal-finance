2ce2cdfb6cd593d70da0871701ae3c72ef277cde
Iskander Idilov <idiloviskander@gmail.com>
2026-05-28 22:04:16 +0500

    fix transactions type

--- src/main/java/finance/repository/TransactionRepository.java
+++ src/main/java/finance/repository/TransactionRepository.java
@@ -1,7 +1,15 @@
 package finance.repository;
 import finance.entity.Transaction;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
 public interface TransactionRepository extends JpaRepository <Transaction, Long> {
-    Long id(Long id);
+
+    @Query("SELECT t FROM Transaction t WHERE " +
+            "LOWER(t.category.name) LIKE LOWER(CONCAT('%', :search, '%')) OR " +
+            "CAST(t.type AS string) LIKE UPPER(CONCAT('%', :search, '%')) OR " +
+            "CAST(t.amount AS string) LIKE CONCAT('%', :search, '%')")
+    List<Transaction> search(@Param("search") String search);
 }
