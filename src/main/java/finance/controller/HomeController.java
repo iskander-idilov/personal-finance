d3eb46dc60d0a5bcb8dca32af5a3264d4750ab6f
Iskander Idilov <idiloviskander@gmail.com>
2026-06-10 06:57:33 +0500

    add Home UI

--- src/main/java/finance/controller/HomeController.java
+++ src/main/java/finance/controller/HomeController.java
@@ -1,11 +1,25 @@
 package finance.controller;
+import finance.service.AccountService;
+import finance.service.TransactionService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final AccountService accountService;
+    private final TransactionService transactionService;
+
     @GetMapping("/")
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("account", accountService.getAccountDto());
+        model.addAttribute("transactions" , transactionService.getTransactions());
+        model.addAttribute("totalIncome", transactionService.getTotalIncome());
+        model.addAttribute("totalExpense", transactionService.getTotalExpense());
+
         return "home";
     }
 }
--- src/main/java/finance/service/impl/TransactionServiceImpl.java
+++ src/main/java/finance/service/impl/TransactionServiceImpl.java
@@ -1,7 +1,10 @@
 package finance.service.impl;
 import finance.dto.TransactionDTO;
 import finance.dto.TransactionMapperMS;
+import finance.entity.Account;
 import finance.entity.Transaction;
+import finance.entity.TransactionType;
+import finance.repository.AccountRepository;
 import finance.repository.TransactionRepository;
 import finance.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,11 +19,24 @@ public class TransactionServiceImpl implements TransactionService {
 
     @Autowired
     private TransactionMapperMS mapper;
+    @Autowired
+    private AccountRepository accountRepository;
+
     @Override
     public List<TransactionDTO> getTransactions(){return transactionRepository.findAll().stream().map(mapper::toDto).collect(Collectors.toList());}
     @Override
     public void addTransaction(Transaction transaction){
         transactionRepository.save(transaction);
+
+        Account account = accountRepository.findAll().get(0);
+
+        if (transaction.getType() == TransactionType.INCOME){
+            account.setBalance(account.getBalance() + transaction.getAmount());
+        } else {
+            account.setBalance(account.getBalance() - transaction.getAmount());
+        }
+
+        accountRepository.save(account);
     }
     @Override
     public Transaction findById(Long id){return transactionRepository.findById(id).orElse(null);}
@@ -30,4 +46,9 @@ public class TransactionServiceImpl implements TransactionService {
     public void save(Transaction transaction){transactionRepository.save(transaction);}
     @Override
     public List<Transaction> search(String search){return transactionRepository.search(search);}
+    @Override
+    public double getTotalIncome() {return transactionRepository.findAll().stream().filter(t -> t.getType() == TransactionType.INCOME).mapToDouble(Transaction::getAmount).sum();}
+    @Override
+    public double getTotalExpense() {return transactionRepository.findAll().stream().filter(t -> t.getType() == TransactionType.EXPENSE).mapToDouble(Transaction::getAmount).sum();}
+
 }
