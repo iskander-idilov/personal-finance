ed423274bfc6f137c08ad1b3ae6a38c6024e7a5d
Iskander Idilov <idiloviskander@gmail.com>
2026-06-10 08:09:09 +0500

    add diagram

--- src/main/java/finance/service/TransactionService.java
+++ src/main/java/finance/service/TransactionService.java
@@ -2,14 +2,16 @@ package finance.service;
 import finance.dto.TransactionDTO;
 import finance.entity.Transaction;
 import java.util.List;
+import java.util.Map;
 
 public interface TransactionService {
     List<TransactionDTO> getTransactions();
     void addTransaction(Transaction transaction);
     Transaction findById(Long id);
-    void removeTransaction(Long id);
+    void removeTransaction(Long id, Transaction transaction);
     void save(Transaction transaction);
     List<Transaction> search(String search);
     double getTotalIncome();
     double getTotalExpense();
+    Map<String, Double> getExpensesByCategory();
 }
