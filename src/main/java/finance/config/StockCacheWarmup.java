2efbb85bd4eb0b63ff48301ac75174b7dcbaa328
Iskander Idilov <idiloviskander@gmail.com>
2026-06-24 17:10:39 +0500

    unit tests

--- src/main/java/finance/config/StockCacheWarmup.java
+++ src/main/java/finance/config/StockCacheWarmup.java
@@ -2,11 +2,13 @@ package finance.config;
 import finance.service.StockService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
+@Profile("!test")
 public class StockCacheWarmup {
 
     private final StockService stockService;
---
+++ src/test/java/finance/AppTests.java
@@ -0,0 +1,10 @@
+package finance;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+public class AppTests {
+    @Test
+    void contextLoads(){
+
+    }
+}
